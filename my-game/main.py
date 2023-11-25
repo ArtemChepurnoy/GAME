62136c292ffe10decccb906a7b485dfcc5417cb9
Tema <email>
2023-11-25 18:55:47 +0300

    add player

--- my-game/main.py
+++ my-game/main.py
@@ -2,7 +2,7 @@ import pygame as pg
 import sys
 from settings import *
 from map import *
-
+from player import *
 class Game:
     def __init__(self) -> None:
         pg.init()     #Обезательно
@@ -10,39 +10,38 @@ class Game:
         self.clock = pg.time.Clock()
         self.delta_time = 1
         self.level = 1
-        #self.new_game()
+        self.new_game()
         #self.sounds = Sound(self)
         #pg.mixer.music.play(-1)
 
     def new_game(self):
         self.delta_time = self.clock.tick(FPS)
-        self.raycasting = RayCasting(self)
+#        self.raycasting = RayCasting(self)
         self.map = Map(self)
-        self.player = Player(self)
-        self.textures = Textures(self)
-        self.npc_control = NpcControl(self)
-        self.finish = Finish(self)
-        self.pathfinding = PathFinding(self)
+        self.player = player(self)
+#        self.textures = Textures(self)
+#        self.npc_control = NpcControl(self)
+#        self.finish = Finish(self)
+#        self.pathfinding = PathFinding(self)
 
     def update(self):
         #self.raycasting.update()
         pg.display.flip()
-        #self.player.update()
+        self.player.update()
         self.delta_time = self.clock.tick(FPS)
         pg.display.set_caption(f'{self.clock.get_fps() :.0f} danya_lox')
         #self.finish.update()
 
     def draw(self):
         self.screen.fill('black')
-        Map(game=self).draw()
-        #self.map.draw()
-        #self.player.draw()
+        self.map.draw()
+        self.player.draw()
         #self.npc_control.update()
         #self.finish.draw()
 
     def check_event(self):
        for event in pg.event.get():
-            if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
+            if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_f):
                 pg.quit()
                 sys.exit()
 
